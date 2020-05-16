ee1f952fa93f5d8af993bea2e233995e15064659
Walter Schwägerl <walter@Walters-MBP-8.fritz.box>
2020-05-16 12:57:44 +0200

    Updates

---
+++ precache-manifest.21f60ce6cdd7e4fb01e164480610d79c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa962aef3930f5b5605e2b1eff5450e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "89472d37bdac24623b80",
+    "url": "/monster-rolodex/static/css/main.8ad6e83d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89472d37bdac24623b80",
+    "url": "/monster-rolodex/static/js/main.19655c3b.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
